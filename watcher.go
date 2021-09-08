813b70085917af52e7c117d98cea740d1af64e60
Peter Teich <peter.teich@gmail.com>
2021-09-08 20:28:52 +0200

    Add watch key function

--- watcher.go
+++ watcher.go
@@ -59,3 +59,44 @@ func (w *Watcher) WatchTree(ctx context.Context, path string) (<-chan consul.KVP
 
 	return out, nil
 }
+
+// WatchKey watches for changes to a key and emits a key value pair
+func (w *Watcher) WatchKey(ctx context.Context, key string) (<-chan *consul.KVPair, error) {
+	out := make(chan *consul.KVPair)
+
+	opts := &consul.QueryOptions{
+		AllowStale:        true,
+		RequireConsistent: false,
+		UseCache:          true,
+	}
+
+	go func() {
+		defer func() {
+			close(out)
+		}()
+
+		for {
+			kvPair, meta, err := w.consul.KV().Get(key, opts.WithContext(ctx))
+			if err != nil {
+				if consul.IsRetryableError(err) {
+					opts.WaitIndex = 0
+					time.Sleep(w.retryTime)
+				} else {
+					return
+				}
+			} else {
+				out <- kvPair
+				opts.WaitIndex = meta.LastIndex
+				opts.WaitTime = w.waitTime
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+		}
+	}()
+
+	return out, nil
+}

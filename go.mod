a9d7bc36a9c8d98439a099b359af1a12b39f0101
Peter Teich <peter.teich@gmail.com>
2021-12-26 17:46:35 +0100

    Use exponential backoff

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/pteich/consul-kv-watcher
 go 1.16
 
 require (
-	github.com/armon/go-metrics v0.3.9 // indirect
-	github.com/fatih/color v1.12.0 // indirect
-	github.com/hashicorp/consul/api v1.10.1
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.2
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/hashicorp/consul/api v1.12.0
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-hclog v0.16.2 // indirect
+	github.com/hashicorp/go-hclog v1.0.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/mattn/go-isatty v0.0.13 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
--- watcher.go
+++ watcher.go
@@ -4,19 +4,26 @@ import (
 	"context"
 	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	consul "github.com/hashicorp/consul/api"
 )
 
 // Watcher is a wrapper around the Consul client that watches for changes to a keys and directories
 type Watcher struct {
-	consul    *consul.Client
-	retryTime time.Duration
-	waitTime  time.Duration
+	consul   *consul.Client
+	backoff  *backoff.ExponentialBackOff
+	waitTime time.Duration
 }
 
 // New returns a new Watcher
 func New(consulClient *consul.Client, waitTime time.Duration, retryTime time.Duration) *Watcher {
-	return &Watcher{consul: consulClient, waitTime: waitTime, retryTime: retryTime}
+	bf := backoff.NewExponentialBackOff()
+	bf.InitialInterval = retryTime
+	return &Watcher{
+		consul:   consulClient,
+		waitTime: waitTime,
+		backoff:  bf,
+	}
 }
 
 // WatchTree watches for changes to a directory and emit key value pairs
@@ -35,25 +42,27 @@ func (w *Watcher) WatchTree(ctx context.Context, path string) (<-chan consul.KVP
 		}()
 
 		for {
-			kvPairs, meta, err := w.consul.KV().List(path, opts.WithContext(ctx))
-			if err != nil {
-				if consul.IsRetryableError(err) {
-					opts.WaitIndex = 0
-					time.Sleep(w.retryTime)
-				} else {
-					return
-				}
-			} else {
-				out <- kvPairs
-				opts.WaitIndex = meta.LastIndex
-				opts.WaitTime = w.waitTime
-			}
-
 			select {
 			case <-ctx.Done():
 				return
 			default:
 			}
+
+			kvPairs, meta, err := w.consul.KV().List(path, opts.WithContext(ctx))
+			if err != nil {
+				if consul.IsRetryableError(err) {
+					opts.WaitIndex = 0
+					time.Sleep(w.backoff.NextBackOff())
+					continue
+				}
+
+				return
+			}
+
+			w.backoff.Reset()
+			out <- kvPairs
+			opts.WaitIndex = meta.LastIndex
+			opts.WaitTime = w.waitTime
 		}
 	}()
 
@@ -76,25 +85,27 @@ func (w *Watcher) WatchKey(ctx context.Context, key string) (<-chan *consul.KVPa
 		}()
 
 		for {
-			kvPair, meta, err := w.consul.KV().Get(key, opts.WithContext(ctx))
-			if err != nil {
-				if consul.IsRetryableError(err) {
-					opts.WaitIndex = 0
-					time.Sleep(w.retryTime)
-				} else {
-					return
-				}
-			} else {
-				out <- kvPair
-				opts.WaitIndex = meta.LastIndex
-				opts.WaitTime = w.waitTime
-			}
-
 			select {
 			case <-ctx.Done():
 				return
 			default:
 			}
+
+			kvPair, meta, err := w.consul.KV().Get(key, opts.WithContext(ctx))
+			if err != nil {
+				if consul.IsRetryableError(err) {
+					opts.WaitIndex = 0
+					time.Sleep(w.backoff.NextBackOff())
+					continue
+				}
+
+				return
+			}
+
+			w.backoff.Reset()
+			out <- kvPair
+			opts.WaitIndex = meta.LastIndex
+			opts.WaitTime = w.waitTime
 		}
 	}()
 
